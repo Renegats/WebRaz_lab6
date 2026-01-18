ed4f8a1ff4c7b7c68c7de520cec4c0298a32245d
Renegats <thebrainbesthappytown@gmail.com>
2026-01-18 20:31:05 +0500

    лаба6

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 
 const router = require("./routers/router");
 const errorRouter = require("./routers/errorRouter");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
